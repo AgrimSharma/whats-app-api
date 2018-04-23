da318f2cc6c7e8f78e4cd95fb1710a76f42ee35e
agrim <agrim.sharma@sirez.com>
2018-04-23 13:13:33 +0530

    udpaetd

--- home/views.py
+++ home/views.py
@@ -7,7 +7,7 @@ import json
 @csrf_exempt
 def home(request):
     if request.method == 'GET':
-        return render(request, "home.html", {"title": "Manohar Lal Jewellers"})
+        return render(request, "home.html", {"title": "WhatsApp API"})
     else:
         phone = request.POST.get('phone')
         text = request.POST.get('message')
