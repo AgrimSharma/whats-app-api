4195bc9ce5bfcd05ab30fe12af33d473520bd2aa
agrim <agrim.sharma@sirez.com>
2018-04-23 13:04:14 +0530

    updated files

---
+++ local.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+source ./setup.sh
+
+function run_locally {
+    python manage.py migrate
+    gunicorn jewellery.wsgi -b 0.0.0.0:$PORT --access-logfile - --error-logfile - --log-level debug
+}
+
+eval_in_virtual_environment run_locally
